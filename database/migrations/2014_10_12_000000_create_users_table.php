feea6d579515b605253f5686b8e32c08b3e9d965
Putri Nurifa <putrinurifa818@gmail.com>
2021-06-05 16:16:51 +0700

    Membuat database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
+            $table->text('alamat');
+            $table->string('no_hp', 15);
+            $table->enum('role', ['adm', 'usr'])->default('usr');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
